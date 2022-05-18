aa219cc7714ef8e2c8a48b4778cc47812e02f6b3
LeeSeonyong <qnfto7898@naver.com>
2022-05-18 17:48:28 +0900

    backtick

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
+const greeting = document.querySelector("#greeting");
+const HIDDEN_CLASSNAME = "hidden";
 
-function onLoginBtnClick() {
-  console.log("click!!!", loginInput.value);
-}
-loginButton.addEventListener("click", onLoginBtnClick)
+function onLoginSubmit(event) {
+  event.preventDefault();
+  const username = loginInput.value
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  // greeting.innerText = "Hello " + username;
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+};
+
+loginForm.addEventListener("submit", onLoginSubmit);
