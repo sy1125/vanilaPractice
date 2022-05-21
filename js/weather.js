f2820c551d2a91ba8eb9319b9a91ab47d24c9190
Leeseonyong <qnfto7898@naver.com>
2022-05-21 18:40:44 +0900

    add Style

--- js/weather.js
+++ js/weather.js
@@ -5,13 +5,13 @@ function onGeoOk(position) {
   const lon = position.coords.longitude;
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
   fetch(url)
-  .then((response) => response.json())
-  .then((data) => {
-    const weather = document.querySelector("#weather span:first-child");
-    const city = document.querySelector("#weather span:last-child");
-    city.innerText = data.name;
-    weather.innerText = `${data.weather[0].main} / ${data.main.temp}degree`;
-  });
+    .then((response) => response.json())
+    .then((data) => {
+      const city = document.querySelector("#weather span:first-child");
+      city.innerText = `Where : ${data.name}\n`;
+      const weather = document.querySelector("#weather span:last-child");
+      weather.innerText = `Today's Weather  : ${data.weather[0].main}`;
+    });
 }
 function onGeoError() {
   console.log("Can`t find your position");
